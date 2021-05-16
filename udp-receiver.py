33ae55222488aaff0bacc824e237a4ac4965f11c
leeyoungseok <yslee@DESKTOP-5M3M7K9.localdomain>
2021-05-16 17:39:12 +0900

    update server codegit add .!

--- udp-receiver.py
+++ udp-receiver.py
@@ -6,6 +6,7 @@ UDP_PORT = 5001
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.bind((UDP_IP, UDP_PORT))
 
+print("UDP Server started !")
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("received message:", data.decode('utf8'), " from ", addr)
