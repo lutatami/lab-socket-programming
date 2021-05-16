9eb2eeb7a21a238abfd33e45c51cfbe967c1faf0
leeyoungseok <yslee@DESKTOP-5M3M7K9.localdomain>
2021-05-16 17:52:24 +0900

    update server codegit add .!

--- udp-receiver.py
+++ udp-receiver.py
@@ -9,6 +9,6 @@ sock.bind((UDP_IP, UDP_PORT))
 print("UDP Server started !")
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    data = data.decode('utf8') + "updated server"
+    data = data + "updated server"
     print("received message:", data.decode('utf8'), " from ", addr)
     sock.sendto(data, addr)
