5e56e43f96b7e5ba8c05f4c4c642cffdb7cfb902
leeyoungseok <yslee@DESKTOP-5M3M7K9.localdomain>
2021-05-16 14:19:53 +0900

    udp-receiver update

--- udp-receiver.py
+++ udp-receiver.py
@@ -1,7 +1,7 @@
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5001
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.bind((UDP_IP, UDP_PORT))
