a30a8b1a93bcf5b0505558a91e10c1217a255404
Youngseok Lee <lee@cnu.ac.kr>
2022-01-15 21:45:02 +0900

    new

--- udp-receiver.py
+++ udp-receiver.py
@@ -10,10 +10,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 print("20220115 New version of UDP server started !")
-TEST_STR = "20220115 "
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("Update received message:", data.decode('utf8'), " from ", addr)
     currentTime = " " + time.ctime(time.time()) + "\r\n"
-    data = TEST_STR.encode('ascii') + data + currentTime.encode('ascii')
+    rcv_str = "update recv: "
+    data = rcv_str.encode('ascii') + data + currentTime.encode('ascii')
     sock.sendto(data, addr)
