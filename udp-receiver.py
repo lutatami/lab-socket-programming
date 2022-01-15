397957b76f2947f9efaf8e422c8d3d744823ab04
leeyoungseok <lee@cnu.ac.kr>
2022-01-15 19:19:46 +0900

    update udp receiver.py

--- udp-receiver.py
+++ udp-receiver.py
@@ -9,7 +9,7 @@ UDP_PORT = 5001
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.bind((UDP_IP, UDP_PORT))
 
-print("New version of UDP server started !")
+print("20220115 New version of UDP server started !")
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("Update received message:", data.decode('utf8'), " from ", addr)
