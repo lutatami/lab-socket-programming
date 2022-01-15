7b36877aff9c7d5d023125b259fcc4e51f756bda
Youngseok Lee <lee@cnu.ac.kr>
2022-01-15 20:44:51 +0900

    test

--- udp-receiver.py
+++ udp-receiver.py
@@ -9,7 +9,7 @@ UDP_PORT = 5001
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.bind((UDP_IP, UDP_PORT))
 
-print("20220115 20:04 New version of UDP server started !")
+print("20220115 New version of UDP server started !")
 TEST_STR = "20220115 "
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
