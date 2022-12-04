323f31b9592664c39e9765166db1d6337f670d51
Youngseok Lee <lee@cnu.ac.kr>
2022-12-04 11:04:24 +0900

    new test

--- udp-receiver.py
+++ udp-receiver.py
@@ -12,7 +12,7 @@ sock.bind((UDP_IP, UDP_PORT))
 print("20221204 Computer Network: AWS testing ! A UDP server started !")
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("Received message (2022) :", data.decode('utf8'), " from ", addr)
+    print("Received message:", data.decode('utf8'), " from ", addr)
     currentTime = " " + time.ctime(time.time()) + "\r\n"
     data = data + currentTime.encode('ascii')
     sock.sendto(data, addr)
