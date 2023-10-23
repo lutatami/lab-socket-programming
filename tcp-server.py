d87eeb1d653f1a0fc597c56c94b894a13e2a2648
Youngseok Lee <lee@cnu.ac.kr>
2023-10-23 16:41:40 +0900

    update 3

--- tcp-server.py
+++ tcp-server.py
@@ -17,7 +17,7 @@ while 1:
      print('Client address:', addr)
      data = conn.recv(BUFFER_SIZE)
      #if not data: break
-     currentTime = " " + " new server2 !!! " + time.ctime(time.time()) + "\r\n"
+     currentTime = " " + " new server 3 !!! " + time.ctime(time.time()) + "\r\n"
      print(data.decode('utf-8'))
      data = data + currentTime.encode('ascii')
      conn.send(data)  # echo
