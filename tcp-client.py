5e6370d531cda6785a596694ff3817fbf39136f3
Youngseok Lee <lee@cnu.ac.kr>
2023-10-23 15:03:14 +0900

    new update

--- tcp-client.py
+++ tcp-client.py
@@ -2,7 +2,7 @@
 #-*- coding:utf-8 -*-
 import socket
 
-IP = '34.64.38.142'
+IP = input("Server address: ")
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = ''
@@ -10,7 +10,7 @@ MESSAGE = ''
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((IP, TCP_PORT))
-    MESSAGE = input('Input: ')
+    MESSAGE = input('Write to server : ')
     if(MESSAGE=='q'):
         break
     s.send(MESSAGE.encode('utf-8'))
